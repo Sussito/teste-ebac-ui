54cd516503bca7bbfbf8349778acd9d20574bb8a
Gabriel <gjxcontabilidade@gmail.com>
2022-10-14 08:14:48 -0300

    otimizando meu código

--- cypress/e2e/produtos.cy.js
+++ cypress/e2e/produtos.cy.js
@@ -1,5 +1,32 @@
-describe('empty spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
-})
+/// <reference types="cypress" />
+
+describe('Funcionalidade Página de Produtos', () => {
+
+    beforeEach(() => {
+      cy.visit('http://lojaebac.ebaconline.art.br/produtos/')
+    });
+
+  it('Deve selecionar um produto da lista', () => {
+      cy.get('[class="product-block grid"]')
+      //.first()
+      //.last()
+      //.eq(3)
+      .contains('Ajax Full-Zip Sweatshirt')
+      .click()    
+  });
+
+  it.only('Deve adicionar um produto ao carrinho', () => {
+      var quantidade = 135
+
+    cy.get('[class="product-block grid"]')
+      .contains('Atlas Fitness Tank').click()
+    cy.get('.button-variable-item-XL').click()
+    cy.get(':nth-child(2) > .value > .variable-items-wrapper > .variable-item').click()
+    cy.get('.input-text').clear().type(quantidade)
+    cy.get('.single_add_to_cart_button').click()
+    cy.get('.dropdown-toggle > .mini-cart-items').should('contain', quantidade)
+    cy.get('.woocommerce-message').should('contain', quantidade + ' × “Atlas Fitness Tank” foram adicionados no seu carrinho.')
+
+
+  });
+});
